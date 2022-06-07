50ec5f3b2dfdb869da768e7a29cf6e2dcfd57d66
Jagadesh <101172104+SJagadeshs1971@users.noreply.github.com>
2022-06-07 12:04:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,130 @@
+Learn with us
+
+RESOURCE LIBRARY
+ENCYCLOPEDIC ENTRY
+
+87
+Buddhism
+Buddhism is one of the world’s largest religions and originated 2,500 years ago in India. Buddhists believe that the human life is one of suffering, and that meditation, spiritual and physical labor, and good behavior are the ways to achieve enlightenment, or nirvana.
+
+GRADES
+
+5 - 8
+
+SUBJECTS
+
+Religion, Social Studies, Storytelling
+
+IMAGE
+
+Buddhist Prayer Candles
+Incense are lit inside of Kun Yam Temple in Macao. Incense and meditation play an important role in Buddhism.
+
+PHOTOGRAPH BY JOE SCHERSCHEL
+Incense are lit inside of Kun Yam Temple in Macao. Incense and meditation play an important role in Buddhism.
+
+
+
+
+ENCYCLOPEDIC ENTRYVOCABULARY
+Buddhism is one of the world’s major religions. It originated in India in 563–483 B.C.E. with Siddhartha Gautama, and over the next millennia it spread across Asia and the rest of the world. Buddhists believe that human life is a cycle of suffering and rebirth, but that if one achieves a state of enlightenment (nirvana), it is possible to escape this cycle forever. Siddhartha Gautama was the first person to reach this state of enlightenment and was, and is still today, known as the Buddha. Buddhists do not believe in any kind of deity or god, although there are supernatural figures who can help or hinder people on the path towards enlightenment.
+
+Siddhartha Gautama was an Indian prince in the fifth century B.C.E. who, upon seeing people poor and dying, realized that human life is suffering. He renounced his wealth and spent time as a poor beggar, meditating and travelling but ultimately, remaining unsatisfied, settling on something called “the Middle Way.” This idea meant that neither extreme asceticism or extreme wealth were the path to enlightenment, but rather, a way of life between the two extremes. Eventually, in a state of deep meditation, he achieved enlightenment, or nirvana underneath the Bodhi tree (the tree of awakening). The Mahabodhi Temple in Bihar, India—the site of his enlightenment—is now a major Buddhist pilgrimage site.
+
+The Buddha taught about Four Noble Truths. The first truth is called “Suffering (dukkha),” which teaches that everyone in life is suffering in some way. The second truth is “Origin of suffering (samudāya).” This states that all suffering comes from desire (tanhā). The third truth is “Cessation of suffering (nirodha),” and it says that it is possible to stop suffering and achieve enlightenment. The fourth truth, “Path to the cessation of suffering (magga)” is about the Middle Way, which are the steps to achieve enlightenment.
+
+Buddhists believe in a wheel of rebirth, where souls are born again into different bodies depending on how they conducted themselves in their previous lives. This is connected to “karma,” which refers to how a person’s good or bad actions in the past or in their past lives can impact them in the future.
+
+There are two main groups of Buddhism: Mahayana Buddhism and Theravada Buddhism. Mahayana Buddhism is common in Tibet, China, Taiwan, Japan, Korea, and Mongolia. It emphasizes the role models of bodhisattvas (beings that have achieved enlightenment but return to teach humans). Theravada Buddhism is common in Sri Lanka, Cambodia, Thailand, Laos, and Burma (Myanmar). It emphasizes a monastic lifestyle and meditation as the way to enlightenment.
+
+Buddhism has been a controversial religion. The head of the Tibetan school of Buddhism and traditional leader of Tibet, the Dalai Lama, fled from China-controlled Tibet in 1959 to India in fear of his life. Many Tibetan Buddhists actively resist Chinese control of the region. Recently, the current Dalai Lama, who is understood to be the fourteenth reincarnation of the first Dalai Lama, has raised questions over whether and where he will choose to reincarnate.
+
+Asceticism
+Noun
+practice of strict self-denial as a measure of personal and especially spiritual discipline
+
+karma
+Noun
+spiritual principle mostly associated with Hinduism and Buddhism, in which the intentions and actions of an individual influence the future of that individual.
+
+meditate
+Verb
+to engage in deep thought, contemplation, or introspection.
+
+monastery
+Noun
+place of residence and worship for a community of religious followers, usually called monks.
+
+nirvana
+Noun
+in the Buddhist religion, an end to personal reincarnations, achieved by the highest enlightenment and freedom from personal passion, hatred, and delusion.
+
+pilgrimage
+Noun
+spiritual journey or travel to a sacred place.
+
+reincarnation
+Noun
+rebirth in new bodies or forms of life
+
+religion
+Noun
+a system of spiritual or supernatural belief.
+
+Credits
+User Permissions
+RELATED RESOURCES
+
+Chinese Religions and Philosophies
+ARTICLE
+Chinese Religions and Philosophies
+Confucianism, Taoism, and Buddhism were the three main philosophies and religions of ancient China, which have individually and collectively influenced ancient and modern Chinese society.
+
+129
+Confucianism
+ENCYCLOPEDIC ENTRY
+Confucianism
+Confucianism is one of the most influential religious philosophies in the history of China, and it has existed for over 2,500 years. It is concerned with inner virtue, morality, and respect for the community and its values.
+
+82
+Jainism
+ENCYCLOPEDIC ENTRY
+Jainism
+Jainism is one of the three ancient religions of India, with a value system that puts nonviolence above all else.
+
+34
+Ancient Civilization: China
+COLLECTION
+Ancient Civilization: China
+Ancient China is responsible for a rich culture, still evident in modern China. From small farming communities rose dynasties such as the Zhou (1046-256 B.C.E), Qin (221-206 B.C.E), and Ming (1368-1644 C.E.). Each had its own contribution to the region. During the Zhou Dynasty, for example, writing was standardized, iron working refined, and famous thinkers like Confucius and Sun-Tzu lived and shared their philosophies. During the Qin Dynasty, Qin Shi Huang commissioned the Terracotta Army, and the Ming Dynasty refurbished the Great Wall to protect the nation from Mongol attacks. Learn more about the history and rich culture of Ancient China with this curated resource collection.
+
+247
+Asian American and Pacific Islander Heritage Month
+COLLECTION
+Asian American and Pacific Islander Heritage Month
+People with ancestral roots in Asia and the islands of the Pacific have been integral to the story of America. To celebrate their heritage and contributions, in 1992, May was designated Asian American and Pacific Islander Heritage Month. The month of May was selected in recognition of the arrival of the first Japanese immigrants in the United States on May 7, 1843, and the significant contributions of Chinese laborers to the completion of North America's transcontinental railroad on May 10, 1869. Currently, those from Asia and the Pacific Islands account for more than 4.7 billion of Earth's 7.9 billion people. And those numbers do not account for the Asian diaspora scattered throughout the rest of the world. Celebrate and learn more about their heritage through this collection. The National Geographic Society acknowledges the increase in discrimination and violence against Asians and Pacific Islanders globally—including here in the United States—in light of the COVID-19 pandemic. Hate and violence toward Asian American and Pacific Islander (AAPI) communities has no place in our world, and we stand with our AAPI communities.
+
+50
+National Geographic Headquarters
+1145 17th Street NW
+Washington, DC 20036
+
+ABOUT
+National Geographic Society
+National Geographic Partners
+News and Impact
+Contact
+EXPLORE
+Our Explorers
+Our Projects
+Resources for Educators
+Museum and Events
+Technology and Innovation
+JOIN US
+Ways to Give
+Apply for a Grant
+Careers
+CONNECT
+National Geographic Society is a 501 (c)(3) organization. © 1996 - 2022 National Geographic Society. All rights reserved
+
+Privacy Notice|Sustainability Policy|Terms of Service|Code of Ethics
